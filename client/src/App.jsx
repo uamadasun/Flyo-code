01d604d3c1fbe9a5e5e144b5d0206d5e9a89de7b
uamadasun <uchennaamadasun@gmail.com>
2023-05-11 09:31:41 -0400

    fixed issue with registration/login forms not showing up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Category from './pages/Category';
-import SiteHeader from './components/SiteHeader';
-import "./index.css"
-import { Login } from './pages/Login';
-import Registration from './pages/Registration';
-import { useState, createContext, useContext } from 'react';
-import { useAuthContext } from './context/AuthContext';
-import { removeToken } from './helpers';
-import Dashboard from './pages/Dashboard';
-
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Category from "./pages/Category";
+import SiteHeader from "./components/SiteHeader";
+import "./index.css";
+import { Login } from "./pages/Login";
+import Registration from "./pages/Registration";
+import { useState, createContext, useContext } from "react";
+import { useAuthContext } from "./context/AuthContext";
+import { removeToken } from "./helpers";
+import Dashboard from "./pages/Dashboard";
 
 export const LoginContext = createContext();
 
 function App() {
-
   const [logged, setLogged] = useState(null);
-  const {user} = useAuthContext();
-
-
+  const { user } = useAuthContext();
 
   return (
     <LoginContext.Provider value={[logged, setLogged]}>
       <div className="App">
-        <SiteHeader>
-        <Routes>
-          <Route exact path="/" element ={<Homepage/>} />
-          <Route path="category/:id"  element ={<Category/>} />
-          <Route path="login"  element ={<Login />} />
-          <Route path="registration"  element ={<Registration/>} />
-          <Route path="dashboard/:id" element = {<Dashboard/>}/>
-
-        </Routes>
-        </SiteHeader>
+        <SiteHeader/>
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="category/:id" element={<Category />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registration" element={<Registration />} />
+            <Route path="dashboard/:id" element={<Dashboard />} />
+          </Routes>
+        
       </div>
     </LoginContext.Provider>
   );
